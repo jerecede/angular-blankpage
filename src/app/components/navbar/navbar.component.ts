a53895f66129a1087bb887ddf4d1843398c62391
jerecede <jeremias10458@gmail.com>
2025-04-29 09:47:09 +0200

    css, mat-icon

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, output } from '@angular/core';
 import { BlankpageService } from '../../services/blankpage.service';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
